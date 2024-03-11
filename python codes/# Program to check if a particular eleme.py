c553f632abaff6a909a60ffad362c708d3b4aa69
adonis2024 <163007357+adonis2024@users.noreply.github.com>
2024-03-11 19:31:26 +0530

    Add files via upload

---
+++ python codes/# Program to check if a particular eleme.py
@@ -0,0 +1,9 @@
+# Program to check if a particular element is present in an array
+
+array = [1, 2, 3, 4, 5]
+target = 5
+
+if target in array:
+    print(target, "is present in the array.")
+else:
+    print(target, "is not present in the array.")
---
+++ python codes/choosing greater.py
@@ -0,0 +1,13 @@
+import re
+
+int(a,b,c)
+print("enter value of a",a)
+print("enter value of b",b)
+print("enter value of c",c)
+
+if a > b >c:
+    print("a is greater",a)
+elif b >c:
+    print ("b is greater",b)
+else :
+    print("c is greater")
